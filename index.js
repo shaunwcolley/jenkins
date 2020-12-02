16e3d6a445bb25ad48da9555e24724ee56d6ed8c
Shaun-Colley <shaun.colley@ibm.com>
2020-12-02 10:41:29 -0600

    updated time to be dynamic

--- index.js
+++ index.js
@@ -1,9 +1,18 @@
 const express = require('express')
 const app = express()
-const port = 8080
+const port = 8081
 
 app.get('/', (req, res) => {
-  res.send('Hello From CPAT 11:48pm!')
+
+  const date = new Date();
+  const month = date.getUTCMonth() + 1;
+  const day = date.getUTCDate();
+  const year = date.getUTCFullYear();
+  const hour  = date.getHours();
+  const min = date.getMinutes();
+  res.send(`Hello GSTA Mainframe App Mod Chapter, Demo ${month}/${day}/${year} at ${hour}:${min} CST!!!`);
+
+  // res.send('Hello GSTA Mainframe App Mod Chapter, Demo 12/2/2020, 10:30am CST!')
 })
 
 app.listen(port, () => {
